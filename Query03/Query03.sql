4cb9973271a2e48f97273ff99220adc25e4d390d
shruthits02 <shruthits02@gmail.com>
2023-10-16 15:33:53 -0400

    update

--- Query03/Query03.sql
+++ Query03/Query03.sql
@@ -1,6 +1,6 @@
 SELECT language_id,film_id, title, description
 FROM film
-WHERE (description ILIKE '%car%' OR description ILIKE '%japan%')
+WHERE (description ILIKE '%car%' OR description ILIKE '%j_p_n%')
 AND language_id IN (SELECT language_id FROM language 
 					WHERE name = 'English' OR name = 'French' OR name = 'Japanese')
 ORDER BY film_id DESC
