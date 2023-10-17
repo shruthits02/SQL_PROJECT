823d2888b01d19f2b87866022704d7483cba2538
shruthits02 <shruthits02@gmail.com>
2023-10-17 10:33:38 -0400

    updated query07 and result

--- Query07/Query07.sql
+++ Query07/Query07.sql
@@ -1,6 +1,11 @@
-SELECT country.country, COUNT(city.city) AS city_count
-FROM city
-JOIN country ON city.country_id = country.country_id
-GROUP BY country.country
-ORDER BY city_count DESC
-LIMIT 5
+SELECT
+    f.film_id,
+    f.title AS film_title,
+    f.rental_duration,
+    (
+        SELECT DENSE_RANK() OVER (ORDER BY rental_duration DESC)
+        FROM film AS subfilm
+        WHERE subfilm.film_id = f.film_id
+    ) AS rental_duration_rank
+FROM film f
+limit 50
